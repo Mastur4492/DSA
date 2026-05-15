c671868a956a1939b9effa0598ab780ddd06912d
Mastur <bikanerwalamastur13@gmail.com>
2026-05-15 20:01:24 +0530

    3 Questions Done

---
+++ Array/MaximumConsecutiveOnes.cpp
@@ -0,0 +1,18 @@
+int consecutiveOnes(vector<int>& arr) {
+
+    int cnt = 0;
+    int maxi = 0;
+
+    for(int i = 0; i < arr.size(); i++) {
+
+        if(arr[i] == 1) {
+            cnt++;
+            maxi = max(maxi, cnt);
+        }
+        else {
+            cnt = 0;
+        }
+    }
+
+    return maxi;
+}
